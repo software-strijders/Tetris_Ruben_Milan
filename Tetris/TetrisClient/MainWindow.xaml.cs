4ef9e23112b203c979582346963a1040aa2033b7
JustMilan <milandol321@gmail.com>
2021-06-04 14:03:17 +0200

    Remove unused imports

--- Tetris/TetrisClient/MainWindow.xaml.cs
+++ Tetris/TetrisClient/MainWindow.xaml.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
-using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
