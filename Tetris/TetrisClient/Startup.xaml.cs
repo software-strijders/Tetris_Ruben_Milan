cf600cff1f5e51dc4e26c226caa935fd596f188a
JustMilan <milandol321@gmail.com>
2021-06-14 23:05:55 +0200

    Add startup with choosing screen

---
+++ Tetris/TetrisClient/Startup.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TetrisClient
+{
+    public partial class Startup : Window
+    {
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleButtonClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var button = (Button) sender;
+            Window window = null;
+            switch (button.Content)
+            {
+                case "Single player":
+                    window = new MainWindow();
+                    break;
+                case "Multiplayer":
+                    window = new MultiplayerWindow();
+                    break;
+                case "Coming Soon":
+                    MessageBox.Show("Comming Soon");
+                    break;
+            }
+
+            Hide();
+            Debug.Assert(window != null, nameof(window) + " != null");
+            window.Closed += (s, args) => Close();
+            window.Show();
+        }
+    }
+}
